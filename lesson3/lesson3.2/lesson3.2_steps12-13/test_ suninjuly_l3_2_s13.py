b3fd6322c6bb8bd483728f033226e5c8637f2ef3
Xsergs <srgsnvsk@gmail.com>
2024-10-17 22:52:45 +0300

    Переименованы файлы по теории, добавлен файл с решением задачи - урок 3.2, шаг 13.

---
+++ lesson3/lesson3.2/lesson3.2_steps12-13/test_ suninjuly_l3_2_s13.py
@@ -0,0 +1,83 @@
+# решение задачи
+
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class SunInJuly(unittest.TestCase):
+    def test_reg1(self):
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration1.html")
+
+        browser.find_element(
+            By.CSS_SELECTOR,
+            "input.form-control.first[placeholder='Input your first name']",
+        ).send_keys("Ivan")
+        browser.find_element(
+            By.CSS_SELECTOR,
+            "input.form-control.second[placeholder='Input your last name']",
+        ).send_keys("Petrov")
+        browser.find_element(
+            By.CSS_SELECTOR, "input.form-control.third[placeholder='Input your email']"
+        ).send_keys("example@example.com")
+        browser.find_element(
+            By.CSS_SELECTOR, "input.form-control.first[placeholder='Input your phone:']"
+        ).send_keys("8-800-555-55-55")
+        browser.find_element(
+            By.CSS_SELECTOR,
+            "input.form-control.second[placeholder='Input your address:']",
+        ).send_keys("Kursk")
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual(
+            welcome_text,
+            welcome_text_elt.text,
+            "Поздравляем! Вы успешно зарегистировались!",
+        )
+
+    def test_reg2(self):
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration2.html")
+
+        browser.find_element(
+            By.CSS_SELECTOR,
+            "input.form-control.first[placeholder='Input your first name']",
+        ).send_keys("Ivan")
+        browser.find_element(
+            By.CSS_SELECTOR,
+            "input.form-control.second[placeholder='Input your last name']",
+        ).send_keys("Petrov")
+        browser.find_element(
+            By.CSS_SELECTOR, "input.form-control.third[placeholder='Input your email']"
+        ).send_keys("example@example.com")
+        browser.find_element(
+            By.CSS_SELECTOR, "input.form-control.first[placeholder='Input your phone:']"
+        ).send_keys("8-800-555-55-55")
+        browser.find_element(
+            By.CSS_SELECTOR,
+            "input.form-control.second[placeholder='Input your address:']",
+        ).send_keys("Kursk")
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual(
+            welcome_text,
+            welcome_text_elt.text,
+            "Поздравляем! Вы успешно зарегистировались!",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
