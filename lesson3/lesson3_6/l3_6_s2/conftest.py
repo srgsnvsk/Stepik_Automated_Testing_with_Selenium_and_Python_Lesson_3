532eb6ca6812847ef833c9d7c0ed340b81fefb1b
Xsergs <srgsnvsk@gmail.com>
2024-10-31 14:43:48 +0300

    Добаылена папка и файл по теории - урок 3.6, шаг 2

---
+++ lesson3/lesson3_6/l3_6_s2/conftest.py
@@ -0,0 +1,16 @@
+# теория
+# conftest.py используется для хранения часто употребимых фикстур и хранения глобальных настроек
+# в файл вынесена фикстура открытия и закрытия браузера
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
