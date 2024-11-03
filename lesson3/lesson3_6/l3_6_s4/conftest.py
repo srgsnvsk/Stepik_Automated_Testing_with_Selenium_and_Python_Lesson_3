ff18c72f16493a6ba2ccb62f48c3f9db08b0882f
Xsergs <srgsnvsk@gmail.com>
2024-11-03 19:08:12 +0300

    Усовершенствована фикстура load_config для запуска теста в VS Code по нажатию кнопочки

--- lesson3/lesson3_6/l3_6_s4/conftest.py
+++ lesson3/lesson3_6/l3_6_s4/conftest.py
@@ -2,6 +2,7 @@
 
 import pytest
 import json
+import os
 from selenium import webdriver
 
 
@@ -14,10 +15,27 @@ def browser():
     browser.quit()
 
 
+# Конструкция с указанием пути к текущему файлу (conftest.py) нужна для Testing средства тестирования VS Code
+# Иначе будет ошибка FileNotFoundError: [Errno 2] No such file or directory: 'config.json'
+# scope="session" будет запущен один раз за всю сессию тестирования
+# Результат скопа session будет доступен для всех тестов, которые используют load_config
 @pytest.fixture(scope="session")
 def load_config():
-    # Открываем файл с конфигом в режиме чтения
-    with open("config.json", "r") as config_file:
-        # С помощью библиотеки json читаем и возвращаем результат
+    # Получаем путь к директории, где лежит conftest.py
+    current_dir = os.path.dirname(__file__)
+    # Строим путь к файлу config.json в этой же директории
+    config_path = os.path.join(current_dir, "config.json")
+    # Открываем файл по пути где находится config.json в режиме чтения
+    with open(config_path, "r") as config_file:
+        # Считываем содержимое файла и сохраняем в переменной
         config = json.load(config_file)
-        return config
+    # Возвращаем загруженные файлы из конфигурации
+    return config
+
+
+# Конструкция без указания пути к текущему файлу, если не нужен запуск по кнопочке в VS Code и достаточно запуска тестов через консоль
+# @pytest.fixture(scope="session")
+# def load_config():
+#     with open("config.json", "r") as config_file:
+#         config = json.load(config_file)
+#     return config
