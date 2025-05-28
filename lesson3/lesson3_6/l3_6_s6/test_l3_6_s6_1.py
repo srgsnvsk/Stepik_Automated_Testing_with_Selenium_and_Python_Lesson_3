7ff4c5505e0d3e5202ae807ae93e52ec5c3d5167
Xsergs <srgsnvsk@gmail.com>
2025-05-28 13:48:32 +0300

    Добавлены файлы по теории - урок 3.6, шаг 7. Добавлено закрытие окна браузера в файл test_l3_6_s6_1.py

--- lesson3/lesson3_6/l3_6_s6/test_l3_6_s6_1.py
+++ lesson3/lesson3_6/l3_6_s6/test_l3_6_s6_1.py
@@ -2,9 +2,11 @@
 # проверка и запуск geckodriver
 
 from selenium import webdriver
+import time
 
 # инициализируем драйвера браузера. 
 # после этой команды откроется новое окно браузера
 driver = webdriver.Firefox()
-
 driver.get("https://stepik.org/lesson/25969/step/8")
+time.sleep(5)
+driver.quit()
